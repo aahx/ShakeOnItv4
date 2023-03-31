a6a5d5c3c58019b418c0c6c5dcd9dcabf31061cb
sparklingwaterlemon <michaelkim009@gmail.com>
2023-03-30 21:22:47 -0700

    created contoller

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var IndexCtrl = require('../controllers/index');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
+// BASE_URL = localhost:3000
+router.get('/', IndexCtrl.index);
 
 module.exports = router;

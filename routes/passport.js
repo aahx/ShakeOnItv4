8c3138bb92178096b0502eebc22245bf0d71addc
sparklingwaterlemon <michaelkim009@gmail.com>
2023-03-30 23:59:24 -0700

    testing outh2callback

--- routes/passport.js
+++ routes/passport.js
@@ -13,8 +13,8 @@ router.get('/google', passport.authenticate(
 router.get('/google/oauth2callback', passport.authenticate(
     'google',
     {
-        successRedirect: '/landing',
-        failureRedirect: '/'
+        successRedirect: '/',
+        failureRedirect: '/error'
     }
 ));
 

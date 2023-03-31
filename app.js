7189d8b26ba60d6983e420563470237e436ace47
sparklingwaterlemon <michaelkim009@gmail.com>
2023-03-30 21:37:36 -0700

    passport

--- app.js
+++ app.js
@@ -8,26 +8,36 @@ var methodOverride = require('method-override');
 var session = require('express-session');
 var passport = require('passport');
 
-// loading env, db, passport
+// Loading env, db, passport
 require('dotenv').config();
 require('./config/passport');
 
-// defining routes
+// Defining routes
 var indexRouter = require('./routes/index');
 var passportRouter = require("./routes/passport");
 
-
 var app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Middlewares default
 app.use(logger('tiny'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Middlewares
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // Routes
 app.use('/', indexRouter);
@@ -36,12 +46,12 @@ app.use("/auth", passportRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};

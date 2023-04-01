3d4088924afaf3f9fe5a6b487557ba2396bed10e
sparklingwaterlemon <michaelkim009@gmail.com>
2023-03-31 19:05:14 -0700

    completed pending. now to display games on landing page.

--- controllers/pendingGame.js
+++ controllers/pendingGame.js
@@ -4,20 +4,19 @@ const UsersDB = require("../models/users");
 async function index(req, res) {
     const pendingGames = await GameDB.find({
         $or: [
-            { player1: req.user._id },
-            { player2: req.user._id }
-        ],
-        status: "pending"
+            { player1: req.user._id, status: { $in: ["pending", "declined"] } },
+            { player2: req.user._id, status: { $in: ["pending", "declined"] } }
+        ]
     });
 
     const user = await UsersDB.findById(req.user._id);
     const userGamertag = user.gamertag;
 
 
-    const gamesData = await Promise.all(pendingGames.map(async(game) => {
+    const gamesData = await Promise.all(pendingGames.map(async (game) => {
         const player1 = await UsersDB.findById(game.player1); // Retrieve player1's gamertag
         const player2 = await UsersDB.findById(game.player2); // Retrieve player2's gamertag
-        const formattedDate = new Date(game.expiration).toLocaleDateString('en-US', {month: '2-digit', day: '2-digit', year: 'numeric'});
+        const formattedDate = new Date(game.expiration).toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' });
 
         return {
             id: game._id,
@@ -25,19 +24,49 @@ async function index(req, res) {
             description: game.description,
             wager: game.wager,
             expiration: formattedDate,
-            player1: player1.gamertag, 
-            player2: player2.gamertag, 
+            player1: player1.gamertag,
+            player2: player2.gamertag,
             status: game.status,
         };
     }));
 
     // console.log(gamesData)
-    res.render('pendingGame',{
+    res.render('pendingGame', {
         gamesData: gamesData,
         userGamertag: userGamertag,
     });
 };
 
+
+async function accept(req, res) {
+    const gameId = req.params.id;
+
+    // Update game status to active
+    await GameDB.findByIdAndUpdate(
+        gameId,
+        { status: 'accepted' },
+        { new: true }
+    );
+
+    res.redirect('/landing');
+};
+
+async function decline(req, res) {
+    const gameId = req.params.id;
+
+    // Update game status to active
+    await GameDB.findByIdAndUpdate(
+        gameId,
+        { status: 'declined' },
+        { new: true }
+    );
+
+    res.redirect('/pending');
+};
+
+
 module.exports = {
     index,
+    accept,
+    decline,
 };
--- routes/pendingGame.js
+++ routes/pendingGame.js
@@ -5,4 +5,10 @@ const PendingGameCtrl = require('../controllers/pendingGame');
 // BASE_URL = localhost:3000/pending
 router.get("/", PendingGameCtrl.index);
 
+// localhost:3000/accept/game._id
+router.get("/accept/:id", PendingGameCtrl.accept);
+
+// localhost:3000/decline/game._id
+router.get("/decline/:id", PendingGameCtrl.decline);
+
 module.exports = router;
